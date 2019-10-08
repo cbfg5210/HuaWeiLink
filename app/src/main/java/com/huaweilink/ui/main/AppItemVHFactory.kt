97831cb739009eadd5d44bfd561ca052d3b1942e
沉璧浮光 <120276969@qq.com>
2019-10-08 10:58:40 +0800

    代码调整

--- app/src/main/java/com/huaweilink/ui/main/AppItemVHFactory.kt
+++ app/src/main/java/com/huaweilink/ui/main/AppItemVHFactory.kt
@@ -1,4 +1,4 @@
-package com.huaweilink.ui
+package com.huaweilink.ui.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
--- app/src/main/java/com/huaweilink/ui/main/MainActivity.kt
+++ app/src/main/java/com/huaweilink/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.huaweilink.ui
+package com.huaweilink.ui.main
 
 import android.os.Bundle
 
