e8a89715f6d83b16622de47cc0ffecb3b8dac33e
沉璧浮光 <120276969@qq.com>
2019-10-08 11:01:56 +0800

    onResume refresh data

--- app/src/main/java/com/huaweilink/ui/main/MainActivity.kt
+++ app/src/main/java/com/huaweilink/ui/main/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.huaweilink.ui.main
 
 import android.os.Bundle
-
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.adapter.BRecyclerAdapter
@@ -11,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: BRecyclerAdapter<JSONObject>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +18,15 @@ class MainActivity : AppCompatActivity() {
 
         rvLinks.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
 
-        BRecyclerAdapter<JSONObject>(this, AppItemVHFactory())
+        adapter = BRecyclerAdapter<JSONObject>(this, AppItemVHFactory())
                 .bindRecyclerView(rvLinks)
-                .setItems(SPHelper.getAppItems())
                 .setItemClickListener { _, item, _ -> }
 
         vgMainRoot.setOnClickListener { finish() }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.setItems(SPHelper.getAppItems()).notifyDataSetChanged()
     }
 }
