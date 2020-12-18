a396b1b86545dbcb2ca4db252e275b938bbdabf0
沉璧浮光 <120276969@qq.com>
2020-12-18 11:21:55 +0800

    remove menu_main

--- app/src/main/java/com/huaweilink/ui/main/MainActivity.kt
+++ app/src/main/java/com/huaweilink/ui/main/MainActivity.kt
@@ -63,19 +63,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 }
     }
 
-    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.actionEdit) {
-            startActivityForResult(Intent(this, AllAppActivity::class.java), REQ_CODE)
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }*/
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQ_CODE && resultCode == Activity.RESULT_OK) {
